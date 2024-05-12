a1602ab59c593a410cccc806b6cef3cd41fa03d8
LeoDr123 <leo.dragic@scv.si>
2024-05-12 16:35:34 +0200

    osnova za eredovalnico

---
+++ src/Skladisce.java
@@ -0,0 +1,42 @@
+
+
+public class Skladisce {
+    // Singleton vzorec
+    private static Skladisce instance;
+    private int uporabnikId;
+    private TipUporabnika tipUporabnika;
+
+    private Skladisce() {
+
+    }
+
+    // Singleton metoda za dostop do instance
+    public static synchronized Skladisce getInstance() {
+        if (instance == null) {
+            instance = new Skladisce();
+        }
+        return instance;
+    }
+
+    public void setTipUporabnika(TipUporabnika tipUporabnika) {
+        this.tipUporabnika = tipUporabnika;
+    }
+
+    public TipUporabnika getTipUporabnika() {
+        return tipUporabnika;
+    }
+
+    public void setUporabnikId(int uporabnikId) {
+        this.uporabnikId = uporabnikId;
+    }
+
+    public int getUporabnikId() {
+        return uporabnikId;
+    }
+
+    private enum TipUporabnika {
+        ADMINISTRATOR,
+        UCITELJ,
+        STARS
+    }
+}
---
+++ src/ZacetnaStran.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ZacetnaStran {
+
+    private JFrame window;
+    private Container container;
+
+    private JLabel mainTitle;
+
+    public ZacetnaStran() {
+        window = new JFrame("Eredovalnica"); // Ustvarimo novo okno
+        window.setPreferredSize(new Dimension(1024, 768)); // Nastavimo velikost okna
+        window.setBounds(10, 10, 1024, 768); // Nastavimo pozicijo in velikost okna
+        window.setLayout(null); // Nastavimo postavitev okna
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Nastavimo akcijo ob zaprtju okna
+        window.setLocationRelativeTo(null); // Nastavimo pozicijo okna na sredino
+        window.setResizable(false); // Omogočimo spreminjanje velikosti okna
+
+        container = window.getContentPane(); // Ustvarimo nov container
+
+        mainTitle = new JLabel("Eredovalnica"); // Ustvarimo nov label
+        mainTitle.setFont(new Font("Arial", Font.BOLD, 48)); // Nastavimo velikost in obliko pisave
+        mainTitle.setBounds(10, 50, 1004, 50); // Nastavimo pozicijo in velikost
+        container.add(mainTitle); // Dodamo label v container
+    }
+
+    public void show() {
+        window.setVisible(true);
+    }
+}
